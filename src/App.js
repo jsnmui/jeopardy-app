c345018789c82414b7a1daf89b59b7239a7bebe1
unknown <jasonmui2004@gmail.com>
2022-07-07 05:43:01 -0400

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import "./App.css";
-import { Component } from "react";
+import {Component} from "react";
 import TriviaData from "./components/Triviadata"
 
 class App extends Component {
   state = {
     searchURL: "http://jservice.io/api/random",
-    trivia: null
-  };
+    trivia: null,
+    };
 
   handleChange = (event) => {
     this.setState({ [event.target.id]: event.target.value })
@@ -15,7 +15,7 @@ class App extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    this.setState( () => {
+    this.setState(  () => {
       fetch(this.state.searchURL)
       .then(res => res.json())
       .then(json => this.setState({trivia:json}))
@@ -28,9 +28,8 @@ class App extends Component {
         <h1>Welcome to Jeopardy!</h1>
         <h3> Let's Play!</h3>
         <button onClick={this.handleSubmit}>Get Question</button>
-          
         {this.state.trivia && <TriviaData trivia={this.state.trivia}/>}
-     
+       
        </div>
     );
   }
